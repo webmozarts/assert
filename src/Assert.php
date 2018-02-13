8269c80ccad36e663f64654875fc48d98a2ee168
Rokas Mikalkėnas <rokas.mikalkenas@nfq.lt>
2018-02-13 17:41:42 +0200

    Changed keyExists structure.

--- src/Assert.php
+++ src/Assert.php
@@ -888,7 +888,7 @@ class Assert
 
     public static function keyExists($array, $key, $message = '')
     {
-        if (!array_key_exists($key, $array)) {
+        if (!(isset($array[$key]) || array_key_exists($key, $array))) {
             static::reportInvalidArgument(sprintf(
                 $message ?: 'Expected the key %s to exist.',
                 static::valueToString($key)
