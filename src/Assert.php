72be1e2919c768818055bab976a97dcf854c8a53
Guilliam Xavier <guilliamxavier@users.noreply.github.com>
2019-02-23 16:41:04 +0100

    re-apply the fix

--- src/Assert.php
+++ src/Assert.php
@@ -1173,7 +1173,7 @@ class Assert
             return strlen($value);
         }
 
-        return mb_strwidth($value, $encoding);
+        return mb_strlen($value, $encoding);
     }
 
     protected static function reportInvalidArgument($message)
