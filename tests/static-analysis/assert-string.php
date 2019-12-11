596100e35e32a618601a397adc0729531a2499f3
Marco Pivetta <ocramius@gmail.com>
2019-12-11 16:44:15 +0100

    Ensure that the lack of usage of the result of a pure operation such as `Assert::string()` will not be considered "redundant"
    Ref: https://github.com/vimeo/psalm/issues/2456
    Ref: https://github.com/vimeo/psalm/commit/4b715cdbffea19a7eab6f72482027d2dd358aab2
    Ref: https://github.com/webmozart/assert/pull/160#issuecomment-564491986

--- tests/static-analysis/assert-string.php
+++ tests/static-analysis/assert-string.php
@@ -14,3 +14,18 @@ function consume($value): string
 
     return $value;
 }
+
+/**
+ * @psalm-pure
+ * @psalm-param mixed $value
+ *
+ * @see https://github.com/webmozart/assert/pull/160#issuecomment-564491986
+ * @see https://github.com/vimeo/psalm/commit/4b715cdbffea19a7eab6f72482027d2dd358aab2
+ * @see https://github.com/vimeo/psalm/issues/2456
+ */
+function consumeWillNotBeRedundantIfAssertingAndNotUsingEither($value): bool
+{
+    Assert::string($value);
+
+    return true;
+}
