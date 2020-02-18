3d3d19e8e0e21984708e40aa59292d61767f7e15
Marco Pivetta <ocramius@gmail.com>
2020-02-18 18:23:13 +0100

    `Assert::same()` is not supposed to couple two types, since the types may already be the same
    If the types are already the same, then the type checker would expand the assertion into a redundant
    condition (unexpected)

--- tests/static-analysis/assert-same.php
+++ tests/static-analysis/assert-same.php
@@ -2,38 +2,18 @@
 
 namespace Webmozart\Assert\Tests\StaticAnalysis;
 
-use stdClass;
 use Webmozart\Assert\Assert;
 
 /**
  * @psalm-pure
- * @psalm-param mixed $value
+ *
+ * @param mixed $value
+ *
+ * @return mixed
  */
-function consume($value): string
+function consume($value)
 {
     Assert::same($value, 'foo');
-
-    return $value;
-}
-
-/**
- * @psalm-pure
- * @psalm-param mixed $value
- */
-function consumeWillInferObject($value): stdClass
-{
-    Assert::same($value, new stdClass());
-
-    return $value;
-}
-
-/**
- * @psalm-pure
- * @psalm-param mixed $value
- */
-function consumeWillInferArray($value): array
-{
-    Assert::same($value, ['foo' => 123]);
 
     return $value;
 }
