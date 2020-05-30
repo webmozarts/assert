052cf791ac93518c2b356d5efafd252fab614e06
Ivan Kurnosov <zerkms@zerkms.com>
2020-05-30 19:49:53 +1200

    Eq tests

--- tests/static-analysis/assert-eq.php
+++ tests/static-analysis/assert-eq.php
@@ -4,9 +4,41 @@ namespace Webmozart\Assert\Tests\StaticAnalysis;
 
 use Webmozart\Assert\Assert;
 
-function consume(string $value): string
+/**
+ * @param mixed $value
+ * @param mixed $expect
+ *
+ * @return mixed
+ */
+function eq($value, $expect)
 {
-    Assert::eq($value, 'foo');
+    Assert::eq($value, $expect);
+
+    return $value;
+}
+
+/**
+ * @param mixed $value
+ * @param mixed $expect
+ *
+ * @return mixed
+ */
+function nullOrEq($value, $expect)
+{
+    Assert::nullOrEq($value, $expect);
+
+    return $value;
+}
+
+/**
+ * @param mixed $value
+ * @param mixed $expect
+ *
+ * @return mixed
+ */
+function allEq($value, $expect)
+{
+    Assert::allEq($value, $expect);
 
     return $value;
 }
