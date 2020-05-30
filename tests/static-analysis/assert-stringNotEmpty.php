30fb79c7fa6967702614618966d9d52312099d3c
Ivan Kurnosov <zerkms@zerkms.com>
2020-05-31 11:12:01 +1200

    stringNotEmpty test

--- tests/static-analysis/assert-stringNotEmpty.php
+++ tests/static-analysis/assert-stringNotEmpty.php
@@ -6,11 +6,42 @@ use Webmozart\Assert\Assert;
 
 /**
  * @psalm-pure
- * @psalm-param mixed $value
+ *
+ * @param mixed $value
+ *
+ * @return non-empty-string
  */
-function consume($value): string
+function stringNotEmpty($value): string
 {
     Assert::stringNotEmpty($value);
+
+    return $value;
+}
+
+/**
+ * @psalm-pure
+ *
+ * @param mixed $value
+ *
+ * @return null|non-empty-string
+ */
+function nullOrStringNotEmpty($value): ?string
+{
+    Assert::nullOrStringNotEmpty($value);
+
+    return $value;
+}
+
+/**
+ * @psalm-pure
+ *
+ * @param mixed $value
+ *
+ * @return iterable<non-empty-string>
+ */
+function allStringNotEmpty($value): iterable
+{
+    Assert::allStringNotEmpty($value);
 
     return $value;
 }
