608d5e5d2b16e2894bfa5f521bb154844778b27a
Ivan Kurnosov <zerkms@zerkms.com>
2020-05-30 20:48:59 +1200

    IsCallable tests

--- tests/static-analysis/assert-isCallable.php
+++ tests/static-analysis/assert-isCallable.php
@@ -6,11 +6,38 @@ use Webmozart\Assert\Assert;
 
 /**
  * @psalm-pure
- * @psalm-param mixed $value
+ *
+ * @param mixed $value
  */
-function consume($value): callable
+function isCallable($value): callable
 {
     Assert::isCallable($value);
+
+    return $value;
+}
+
+/**
+ * @psalm-pure
+ *
+ * @param mixed $value
+ */
+function nullOrIsCallable($value): ?callable
+{
+    Assert::nullOrIsCallable($value);
+
+    return $value;
+}
+
+/**
+ * @psalm-pure
+ *
+ * @param mixed $value
+ *
+ * @return iterable<callable>
+ */
+function allIsCallable($value): iterable
+{
+    Assert::allIsCallable($value);
 
     return $value;
 }
