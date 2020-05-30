524925012b32dded4f5a883040b563556db76efb
Ivan Kurnosov <zerkms@zerkms.com>
2020-05-30 20:08:07 +1200

    Integerish tests

--- tests/static-analysis/assert-integerish.php
+++ tests/static-analysis/assert-integerish.php
@@ -6,13 +6,42 @@ use Webmozart\Assert\Assert;
 
 /**
  * @psalm-pure
- * @psalm-param string|numeric $value
  *
- * @psalm-return numeric
+ * @param mixed $value
+ *
+ * @return numeric
  */
-function consume($value)
+function integerish($value)
 {
     Assert::integerish($value);
+
+    return $value;
+}
+
+/**
+ * @psalm-pure
+ *
+ * @param mixed $value
+ *
+ * @return null|numeric
+ */
+function nullOrIntegerish($value)
+{
+    Assert::nullOrIntegerish($value);
+
+    return $value;
+}
+
+/**
+ * @psalm-pure
+ *
+ * @param mixed $value
+ *
+ * @return iterable<numeric>
+ */
+function allIntegerish($value): iterable
+{
+    Assert::allIntegerish($value);
 
     return $value;
 }
