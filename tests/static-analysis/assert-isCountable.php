21a10467d6e1e9553aaf1779397646cb8f8810c9
Ivan Kurnosov <zerkms@zerkms.com>
2020-05-30 20:51:08 +1200

    IsCountable tests

--- tests/static-analysis/assert-isCountable.php
+++ tests/static-analysis/assert-isCountable.php
@@ -7,13 +7,42 @@ use Webmozart\Assert\Assert;
 
 /**
  * @psalm-pure
- * @psalm-param object $value
+ *
+ * @param mixed $value
+ *
+ * @return countable
  */
-function consume($value): Countable
+function isCountable($value)
 {
     Assert::isCountable($value);
-    count($value);
 
+    return $value;
+}
+
+/**
+ * @psalm-pure
+ *
+ * @param mixed $value
+ *
+ * @return null|countable
+ */
+function nullOrIsCountable($value)
+{
+    Assert::nullOrIsCountable($value);
+
+    return $value;
+}
+
+/**
+ * @psalm-pure
+ *
+ * @param mixed $value
+ *
+ * @return iterable<countable>
+ */
+function allIsCountable($value)
+{
+    Assert::allIsCountable($value);
 
     return $value;
 }
