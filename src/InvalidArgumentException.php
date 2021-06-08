8e93dd180ff7f809286656f9d0edf3c30d3a533b
Eugene Borovov <eugene.borovov@olmisoft.com>
2021-06-08 23:44:36 +0700

    Fix default values in exception constructor

--- src/InvalidArgumentException.php
+++ src/InvalidArgumentException.php
@@ -13,7 +13,7 @@ namespace Webmozart\Assert;
 
 class InvalidArgumentException extends \InvalidArgumentException
 {
-    public function __construct($message = null, $code = null, $previous = null)
+    public function __construct($message = '', $code = 0, $previous = null)
     {
         parent::__construct($message, $code, $previous);
 
