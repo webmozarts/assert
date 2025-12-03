68b79e9eb9072546bc435fbdf641fee49c9a7b71
Woody Gilk <woody.gilk@gmail.com>
2025-12-03 07:30:34 -0600

    Clean up new isStatic/notStatic (#329)

--- tests/static-analysis/assert-notStatic.php
+++ tests/static-analysis/assert-notStatic.php
@@ -5,9 +5,10 @@ namespace Webmozart\Assert\Tests\StaticAnalysis;
 use Closure;
 use Webmozart\Assert\Assert;
 
-function notStatic(Closure $closure): Closure
+/**
+ * @return Closure|callable-string
+ */
+function notStatic(mixed $closure): Closure|string
 {
-    Assert::notStatic($closure);
-
-    return $closure;
+    return Assert::notStatic($closure);
 }
